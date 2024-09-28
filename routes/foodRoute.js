60a9ec3343015cf8219204d83720ca562bda1f9d
Sekh Hasim Ahmed <hasim.cs@gmail.com>
2024-09-28 13:59:18 +0530

    Create uploads directory if not exists.

--- routes/foodRoute.js
+++ routes/foodRoute.js
@@ -1,17 +1,28 @@
 import express from "express";
+const fs = require("fs");
+import multer from "multer";
 import {
   addFood,
   listFood,
   deleteFood,
 } from "../controllers/foodController.js";
 
-import multer from "multer";
-
 const foodRouter = express.Router();
 
 // Image upload
 const storage = multer.diskStorage({
-  destination: "uploads",
+  // destination: "uploads",
+
+  destination: (req, file, cb) => {
+    const dir = "uploads";
+
+    // Check if the directory exists, if not, create it
+    if (!fs.existsSync(dir)) {
+      fs.mkdirSync(dir, { recursive: true });
+    }
+
+    cb(null, dir);
+  },
   filename: (req, file, cb) => {
     return cb(null, `${Date.now()}-${file.originalname}`);
   },
