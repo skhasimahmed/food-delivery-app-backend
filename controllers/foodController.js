b4f756c55c2e2f571d70ecc09ca3124b4342bf88
Sekh Hasim Ahmed <hasim.cs@gmail.com>
2024-09-28 20:41:09 +0530

    Get food and Edit food routes add.

--- controllers/foodController.js
+++ controllers/foodController.js
@@ -1,5 +1,23 @@
 import foodModel from "../models/foodModel.js";
 import fs from "fs";
+import path, { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+// Get food item
+const getFood = async (req, res) => {
+  try {
+    const food = await foodModel.findById(req.params.id);
+    res.json({
+      success: true,
+      data: food,
+      message: "Food fetched successfully",
+    });
+  } catch (error) {
+    res.json({ message: error.message, success: false });
+  }
+};
 
 // Add food item
 const addFood = async (req, res) => {
@@ -41,6 +59,46 @@ const listFood = async (req, res) => {
   }
 };
 
+// Edit food item
+const editFood = async (req, res) => {
+  try {
+    const food = await foodModel.findById(req.params.id);
+
+    if (!food) {
+      return res.json({
+        message: "Food not found",
+        success: false,
+      });
+    }
+
+    if (req.file) {
+      const oldImagePath = path.join(__dirname, `../uploads/${food.image}`);
+
+      // Check if the old file exists before attempting to delete
+      if (fs.existsSync(oldImagePath)) {
+        fs.unlinkSync(oldImagePath);
+      }
+
+      // fs.unlinkSync(`./uploads/${food.image}`);
+      food.image = req.file.filename;
+    }
+
+    food.name = req.body.name;
+    food.price = req.body.price;
+    food.description = req.body.description;
+    food.category = req.body.category;
+
+    await food.save();
+
+    res.json({
+      message: "Food updated successfully",
+      success: true,
+    });
+  } catch (error) {
+    res.json({ message: error.message, success: false });
+  }
+};
+
 // Delete food item
 const deleteFood = async (req, res) => {
   try {
@@ -66,4 +124,4 @@ const deleteFood = async (req, res) => {
   }
 };
 
-export { addFood, listFood, deleteFood };
+export { addFood, getFood, listFood, editFood, deleteFood };
--- routes/foodRoute.js
+++ routes/foodRoute.js
@@ -2,8 +2,10 @@ import express from "express";
 import fs from "fs";
 import multer from "multer";
 import {
+  getFood,
   addFood,
   listFood,
+  editFood,
   deleteFood,
 } from "../controllers/foodController.js";
 
@@ -31,7 +33,9 @@ const storage = multer.diskStorage({
 const upload = multer({ storage: storage });
 
 foodRouter.post("/add", upload.single("image"), addFood);
+foodRouter.get("/:id/get", getFood);
 foodRouter.get("/list", listFood);
+foodRouter.put("/:id/edit", upload.single("image"), editFood);
 foodRouter.delete("/delete/:id", deleteFood);
 
 export default foodRouter;
