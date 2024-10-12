56050256f0133e6549c222d330a99b776fb74b98
Sekh Hasim Ahmed <hasim.cs@gmail.com>
2024-10-12 21:05:47 +0530

    User management APIs.

--- controllers/adminUserController.js
+++ controllers/adminUserController.js
@@ -18,6 +18,63 @@ export const getAllUsers = async (req, res) => {
   }
 };
 
+export const getUser = async (req, res) => {
+  try {
+    const user = await userModel.findById(req.params.id);
+    if (!user) {
+      return res.status(404).json({
+        message: "User not found",
+        success: false,
+      });
+    }
+    res.status(200).json({
+      message: "User fetched successfully",
+      success: true,
+      data: user,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+      success: false,
+    });
+  }
+};
+
+export const updateUser = async (req, res) => {
+  try {
+    const user = await userModel.findById(req.params.id);
+    if (!user) {
+      return res.status(404).json({
+        message: "User not found",
+        success: false,
+      });
+    }
+
+    console.log(req.body);
+
+    user.name = req.body.name || user.name;
+    user.email = req.body.email || user.email;
+    user.stripeCustomerId = req.body.stripeCustomerId || user.stripeCustomerId;
+    user.isAdmin = req.body.isAdmin
+      ? req.body.isAdmin == "1"
+        ? true
+        : false
+      : user.isAdmin;
+
+    await user.save();
+
+    res.status(200).json({
+      message: "User updated successfully",
+      success: true,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+      success: false,
+    });
+  }
+};
+
 export const deleteUser = async (req, res) => {
   try {
     const user = await userModel.findById(req.params.id);
--- routes/adminUserRoute.js
+++ routes/adminUserRoute.js
@@ -1,9 +1,16 @@
 import express from "express";
-import { getAllUsers, deleteUser } from "../controllers/adminUserController.js";
+import {
+  getAllUsers,
+  getUser,
+  updateUser,
+  deleteUser,
+} from "../controllers/adminUserController.js";
 
 const adminUserRouter = express.Router();
 
 adminUserRouter.get("/", getAllUsers);
+adminUserRouter.get("/:id", getUser);
+adminUserRouter.put("/update/:id", updateUser);
 adminUserRouter.delete("/delete/:id", deleteUser);
 
 export default adminUserRouter;
